5e580fa1dfd0ab920c3b7d575e6f598ecd8f4aad
Humberto Lidio Antonelli <humbertoantonelli@usp.br>
2022-09-16 09:47:23 -0400

    Inicialização do exemplo 3

---
+++ exemplo3/app/src/main/java/com/example/navegaoactivities/navegaoactivities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.navegaoactivities.navegaoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
