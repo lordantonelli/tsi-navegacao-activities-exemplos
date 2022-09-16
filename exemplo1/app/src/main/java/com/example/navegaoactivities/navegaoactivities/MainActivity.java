62c039a3915e0ec95c0ea3ceb4a315b70ec8586b
Humberto Lidio Antonelli <humbertoantonelli@usp.br>
2022-09-16 07:55:28 -0400

    Finalização do exemplo 1

--- exemplo1/app/src/main/java/com/example/navegaoactivities/navegaoactivities/MainActivity.java
+++ exemplo1/app/src/main/java/com/example/navegaoactivities/navegaoactivities/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.navegaoactivities.navegaoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnVai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnVai = findViewById(R.id.btnVai);
+
+        this.btnVai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), OutraActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
